0a12d4772f3314e3498ab0700c828a34ed6f141a
Rafael <rafaelfigueiredo14nunes@gmail.com>
2025-11-07 20:12:28 -0300

    Alterações necessárias

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
       {/* Adicione a Navbar aqui */}
-      <Navbar />
+      
       
       <Routes>
         {/* Página inicial */}
