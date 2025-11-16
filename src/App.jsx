f712889e86a59e7783e940f6cb9e664e06b45b41
Rafael <rafaelfigueiredo14nunes@gmail.com>
2025-11-16 12:45:42 -0300

    Configuração de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/landingpage.jsx";
 import Login from "./pages/Login/login.jsx";
 import Register from "./pages/Register/register.jsx";
-import Navbar from "./pages/LandingPage/components/navbar.jsx"; // Importe a Navbar
 import GameHome from "./pages/GameHome/gameHome.jsx";
-import EditarPerfil from "./pages/GameHome/EditarPerfil/editarPerfil.jsx";
+import ProfileView from "./pages/GameHome/ProfileView/profileView.jsx";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-      {/* Adicione a Navbar aqui */}
-      
-      
       <Routes>
         {/* Página inicial */}
         <Route path="/" element={<LandingPage />} />
@@ -23,7 +19,12 @@ function App() {
 
         {/* Game Home */}
         <Route path="/gameHome" element={<GameHome />} />
-        <Route path="/editarPerfil" element={<EditarPerfil />} />
+        
+        {/* Profile - Rota principal do perfil */}
+        <Route path="/profile/:playerId" element={<ProfileView />} />
+        
+        {/* Rota legada para compatibilidade - pode remover depois */}
+        <Route path="/editarPerfil" element={<ProfileView />} />
       </Routes>
     </div>
   );
